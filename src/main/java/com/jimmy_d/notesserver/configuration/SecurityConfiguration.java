98e79c93aeb39d412950d94d61f672e34e763be5
JimmyD <cbrigante@mail.ru>
2025-05-28 19:47:30 +0300

    add additional security to user controller and login/logout в SecurityConfiguration

--- src/main/java/com/jimmy_d/notesserver/configuration/SecurityConfiguration.java
+++ src/main/java/com/jimmy_d/notesserver/configuration/SecurityConfiguration.java
@@ -48,6 +48,18 @@ public class SecurityConfiguration {
                         .permitAll()
                         .anyRequest().authenticated()
                 )
+                .formLogin(login -> login
+                        .loginProcessingUrl("/api/auth/login")
+                        .successHandler((request, response, authentication) -> response.setStatus(200))
+                        .failureHandler((request, response, exception) -> response.setStatus(401))
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/api/auth/logout")
+                        .logoutSuccessHandler((req, res, auth) -> res.setStatus(200))
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                )
                 .build();
     }
 }
