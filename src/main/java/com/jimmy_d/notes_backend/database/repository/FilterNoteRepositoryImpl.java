ff9230b4e6d079cf001cef10dc0e0570dd10e2fe
jimmy d <jonny.cbrigante@gmail.com>
2026-05-13 10:36:52 +0300

    Find bu filter: author case-insensitive fix

--- src/main/java/com/jimmy_d/notes_backend/database/repository/FilterNoteRepositoryImpl.java
+++ src/main/java/com/jimmy_d/notes_backend/database/repository/FilterNoteRepositoryImpl.java
@@ -37,7 +37,7 @@ public class FilterNoteRepositoryImpl implements FilterNoteRepository {
                 .add(filter.title(), title -> note.title.lower().like(title.toLowerCase()))
                 .add(filter.tag(), tag -> note.tag.lower().eq(tag.toLowerCase()))
                 .add(filter.content(), (content -> note.content.lower().like(content.toLowerCase())))
-                .add(filter.author(), note.author.username::eq)
+                .add(filter.author(), (author -> note.author.username.lower().like(author.toLowerCase())))
                 .buildAnd();
 
         return new JPAQuery<Note>(entityManager)
